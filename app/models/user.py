2b187b84057ddd68e687a72d8879d6dc08f94ca6
Frances Huang <frances.y.huang@gmail.com>
2022-06-06 18:17:27 -0700

    push to heroku

--- app/models/user.py
+++ app/models/user.py
@@ -7,9 +7,16 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
+    username = db.Column(db.String(40), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    phone = db.Column(db.String(40))
+    agent = db.Column(db.Boolean(), default=False)
+    license_num = db.Column(db.String(20))
+    bio = db.Column(db.String(2000))
+    photo = db.Column(db.String)
+    broker_license = db.Column(db.String(40))
+    office = db.Column(db.String(100))
 
     @property
     def password(self):
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,15 +4,15 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        username='Demo User', email='demo@aa.io', password='password')
+    frances = User(
+        username='Frances', email='frances@aa.io', password='password')
+    richard = User(
+        username='Richard', email='richard@aa.io', password='password')
 
     db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    db.session.add(frances)
+    db.session.add(richard)
 
     db.session.commit()
 
