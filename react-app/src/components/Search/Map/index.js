b7d41e7c8f987945054630c13ed8a6d7fa6707d3
Frances Huang <frances.y.huang@gmail.com>
2022-06-13 09:33:24 -0700

    added price label but not css yet

--- react-app/src/components/Search/Map/index.js
+++ react-app/src/components/Search/Map/index.js
@@ -13,6 +13,26 @@ const MyMap = withScriptjs(
 	withGoogleMap((props) => {
 		const mapRef = useRef(null);
 
+		const iconPin = {
+			path: "M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8z",
+			fillColor: "#ef3d4d",
+			stroke: "#ffffff",
+			strokeWidth: "2px",
+			fillOpacity: 1,
+			scale: 0.03, //to reduce the size of icons
+		};
+
+		const priceLabel = (price) => {
+			let newPrice;
+			if (price > 1000000) {
+				newPrice = (price / 1000000).toFixed(2);
+				return `${newPrice}M`;
+			} else {
+				newPrice = price / 1000;
+				return `${newPrice}K`;
+			}
+		};
+
 		// Fit bounds function
 		const fitBounds = () => {
 			const bounds = new window.google.maps.LatLngBounds();
@@ -37,14 +57,18 @@ const MyMap = withScriptjs(
 					lng: props.center.lng,
 				}}
 			>
-				{props.markers.map((marker) => (
-					<Marker
-						position={{ lat: marker?.lat, lng: marker?.lng }}
-						key={marker?.id}
-						icon={{ url: dot }}
-						onClick={() => alert(marker)}
-					></Marker>
-				))}
+				{props.markers.map((marker) => {
+					const label = priceLabel(marker?.price);
+					return (
+						<Marker
+							position={{ lat: marker?.lat, lng: marker?.lng }}
+							key={marker?.id}
+							icon={iconPin}
+							onClick={() => alert(marker)}
+							label={label}
+						></Marker>
+					);
+				})}
 			</GoogleMap>
 		);
 	})
