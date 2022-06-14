3a085b2c4bc4f6c2734d56f574859812c8e2cdee
Frances Huang <frances.y.huang@gmail.com>
2022-06-14 09:49:34 -0700

    added cities to service areas

--- app/models/agent_area.py
+++ app/models/agent_area.py
@@ -1,10 +1,17 @@
 from .db import db
+from .property import Property
 
 class AgentArea(db.Model):
     __tablename__ = "agent_areas"
 
     id = db.Column(db.Integer, primary_key=True)
     agent_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    zip = db.Column(db.Integer, nullable=False)
+    zip = db.Column(db.String(5), nullable=False)
 
     agent = db.relationship("User", back_populates="areas")
+
+    def city(self):
+        properties = Property.query.filter(Property.zip == self.zip).all()
+        cities = [property.city for property in properties]
+        uniqueCities = list(set(cities))
+        return {"zip": self.zip, "cities": uniqueCities}
